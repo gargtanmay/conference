0ba453417390af72a08cd8b064d5be251a3db43d
gargtanmay <tanmayjdr@gmail.com>
2021-07-22 12:50:55 +0530

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Footer() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style={{height:"50px"}}>
         <div class="container-fluid">
-          <span class="navbar-brand mb-0 h1">Copyright @ Tanmay</span>
+          <span class="navbar-brand mb-0 h1" ></span>
         </div>
       </nav>
     </div>
